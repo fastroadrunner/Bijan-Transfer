7b77e465c85e8381e1475f59883b1ee76f1eeb9a
Varun <vp9@knights.ucf.edu>
2023-10-21 16:56:07 -0400

    Add sample index page

--- CurrencyTransferProject/settings.py
+++ CurrencyTransferProject/settings.py
@@ -22,6 +22,10 @@ from dotenv import load_dotenv, find_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "users", "templates")
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
